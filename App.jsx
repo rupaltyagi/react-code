c58eeb007eb7028116aad41cb413be7be9c1bd21
Rupal Tyagi <111173719+rupaltyagi@users.noreply.github.com>
2024-06-25 12:08:07 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import Form from "./Form"
+import Calculator from "./Calculator.jsx"
+import Counter from "./Counter.jsx"
+import ColorPicker from "./Colorpicker.jsx"
+import ToDoList from "./ToDoList.jsx"
+import DigitalClock from "./DigitalClock.jsx"
+import Validation from "./Validation.jsx"
+import StopWatch from "./StopWatch.jsx"
+function App() {
+  return <>
+          <Form/>
+           <StopWatch/>
+          </>
+}
+
+export default App
